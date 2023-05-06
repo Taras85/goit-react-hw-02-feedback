0c0fd29ad776d9150ed54b19b61ba1d4930711cd
Taras <topanasenko85@gmail.com>
2023-05-06 17:06:46 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import s from './Statistics.module.css'
 
 const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
-  console.log(positivePercentage)
+  // console.log(positivePercentage)
   return (
     <ul>
       <li>Good: {good}</li>
